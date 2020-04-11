57148b0cb491da3787430fb4e03dd613f9fc2d7d
Roberto Livi <robertomlivi@outlook.com>
2020-04-11 03:33:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,21 +16,16 @@ class ApplicationController < Sinatra::Base
     @article = Article.create(params)
     redirect "/articles/#{@article.id}"
   end
-  
-  get '/articles' do
-    @articles = Article.all
-    erb :index
-  end
 
   get '/articles/:id' do
     @article = Article.find_by_id(params[:id])
     erb :show
   end
   
-  # get '/articles' do
-  #   @articles = Article.all
-  #   erb :index
-  # end
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
   
   # get '/articles/:id/edit' do
   #   @article = Article.find_by_id(params[:id])
