28440a5404571df739ebd2cfe768a5733ede97e5
Roberto Livi <robertomlivi@outlook.com>
2020-04-11 03:53:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ class ApplicationController < Sinatra::Base
   
   delete '/articles/:id/delete' do
     @article = Article.find_by_id(params[:id])
-    @article.clear
+    @article.destroy
     
     erb :deleted
   end
